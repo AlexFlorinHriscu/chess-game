78342987c6fa26ef80ee30158438b9e5348119d0
Florin Hriscu <alexflorin.hriscu@gmail.com>
2020-01-20 23:12:49 +0200

    Create main application class.

---
+++ src/main/java/com/game/chess/ChessApplication.java
@@ -0,0 +1,13 @@
+package com.game.chess;
+
+public class ChessApplication {
+
+    public static void main(String[] args) {
+        new ChessApplication().run();
+    }
+
+    private void run() {
+        System.out.println("App is running !");
+    }
+
+}
